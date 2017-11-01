58fccaf93b3ad3cddd33f1f8aca35c700f10a480
wmk0670 <wmk0670@dhcp-129-105-233-80.it.northwestern.edu>
2017-11-01 16:57:12 -0500

    Initial commit

---
+++ TestProject/src/com/test/helpers/Helper.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.test.helpers;
+
+/**
+ * @author wmk0670
+ *
+ */
+public class Helper {
+
+	/**
+	 * 
+	 */
+	public Helper() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
